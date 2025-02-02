f355bb95616fef6934d19c0211acbe82fb138241
alsongard <alsongard@gmail.com>
2025-02-03 01:10:07 +0200

    data types and outputing data

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -15,6 +15,27 @@ namespace MyApp
 			Console.WriteLine("/  |");
 			Console.WriteLine("/  |");
 			Console.WriteLine("/   |");
+
+
+			// working with variables
+
+			string userName = "Jarvis";
+			int userAge = 25;
+			string location = "Dalas";
+			Console.WriteLine("The young man's name is " +  userName);
+			Console.WriteLine("The young mans age is " + userAge + " years old");
+			Console.WriteLine("The young man lived in " + location);
+
+			char singleLetter = 'c';
+			double luckyNumber =  12.10;
+			bool isMale = true;
+
+			Console.WriteLine("singleLetter value is " + singleLetter);
+			Console.WriteLine("luckyNumber value is " + luckyNumber);
+			Console.WriteLine("Boolean isMale is " + isMale);
+
+
+
     		}	
 	}
 }
