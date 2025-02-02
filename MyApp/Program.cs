2296f1a46a3dbc43de2fab1a8ca1536a7415de3d
alsongard <alsongard@gmail.com>
2025-02-03 00:35:02 +0200

    my first C# program

---
+++ MyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+
+
+	class Program
+	{	
+    		static void Main()
+    		{
+        		Console.WriteLine("Follow, LinuxTLDR");
+
+			Console.WriteLine("  /|");
+			Console.WriteLine(" / |");
+			Console.WriteLine("/  |");
+			Console.WriteLine("/  |");
+			Console.WriteLine("/   |");
+    		}	
+	}
+}
