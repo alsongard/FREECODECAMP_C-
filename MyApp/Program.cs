5d2667d3cc1c80914df425a44aab89c87a8bf842
alsongard <alsongard@gmail.com>
2025-02-14 09:40:06 +0200

    string methods done

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,36 +6,46 @@ namespace MyApp
 
 	class Program
 	{	
-    		static void Main()
-    		{
-        		Console.WriteLine("Follow, LinuxTLDR");
+		static void Main()
+		{
+			Console.WriteLine("Follow, LinuxTLDR");
 
-			Console.WriteLine("  /|");
-			Console.WriteLine(" / |");
-			Console.WriteLine("/  |");
-			Console.WriteLine("/  |");
-			Console.WriteLine("/   |");
+			Console.WriteLine("    /|");
+			Console.WriteLine("   / |");
+			Console.WriteLine("  /  |");
+			Console.WriteLine(" /   |");
+			Console.WriteLine("/	|");
+			Console.WriteLine("------");
+
+		// working with variables
+
+		string userName = "Jarvis";
+		int userAge = 25;
+		string location = "Dalas";
+		Console.WriteLine("The young man's name is " +  userName);
+		Console.WriteLine("The young mans age is " + userAge + " years old");
+		Console.WriteLine("The young man lived in " + location);
+
+		char singleLetter = 'c';
+		double luckyNumber =  12.10;
+		bool isMale = true;
+
+		Console.WriteLine("singleLetter value is " + singleLetter);
+		Console.WriteLine("luckyNumber value is " + luckyNumber);
+		Console.WriteLine("Boolean isMale is " + isMale);
 
 
-			// working with variables
+		string myPhrase = "Good Morning";
+		Console.WriteLine(myPhrase);
 
-			string userName = "Jarvis";
-			int userAge = 25;
-			string location = "Dalas";
-			Console.WriteLine("The young man's name is " +  userName);
-			Console.WriteLine("The young mans age is " + userAge + " years old");
-			Console.WriteLine("The young man lived in " + location);
+		Console.WriteLine(myPhrase.Substring(4));
 
-			char singleLetter = 'c';
-			double luckyNumber =  12.10;
-			bool isMale = true;
+		// indexing
+		Console.WriteLine(myPhrase[5] + "My friend");
+		
+		// the Substring method can also take 2 arguments
+		Console.WriteLine(myPhrase.Substring(5, 3) + "Gardens");
 
-			Console.WriteLine("singleLetter value is " + singleLetter);
-			Console.WriteLine("luckyNumber value is " + luckyNumber);
-			Console.WriteLine("Boolean isMale is " + isMale);
-
-
-
-    		}	
+		}	
 	}
 }
