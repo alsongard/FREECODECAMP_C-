d99ab67e94911a9dee758216e02e7f598ef901ad
alsongard <alsongard@gmail.com>
2025-02-14 11:21:29 +0200

    my calculator

---
+++ calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace calculator
+{
+	class Program
+	{
+		static void Main(string [] args)
+		{
+			Console.Write("Enter full name:");
+			string fullName = Console.ReadLine() ?? "";
+			Console.Write("Enter age: ");
+			string userAge = Console.ReadLine() ?? "";
+
+			Console.WriteLine("Username is : " + fullName + " age is :" + userAge); 
+		}
+	}
+}
