f3595e76107b43ceb36a7b480816320ada7fcf79
alsongard <alsongard@gmail.com>
2025-03-05 23:52:50 +0300

    working with unity

---
+++ UnityExample/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class WorkingVariables
+{
+	static void Main()
+	{
+		float a = 10;
+		float b = 3;
+		float c = a / b;
+
+		Console.WriteLine(a + " / " + b + " = " + c);
+	}
+}
